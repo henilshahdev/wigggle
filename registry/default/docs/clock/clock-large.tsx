28c582a5665435c9f3a3a556aa0dd335c194a86c
henilshahdev <henil.601@gmail.com>
2025-01-13 18:28:30 -0800

    :wrench: Refactor Clock Widget Examples

--- registry/default/docs/clock/clock-large.tsx
+++ registry/default/docs/clock/clock-large.tsx
@@ -5,55 +5,52 @@ import {
 } from '../../wigggle/widget';
 
 export default function ClockDemo() {
+  const weatherInfo = [
+    {
+      id: 1,
+      city: 'Mumbai',
+      time: 'Tuesday',
+      hour: '7:10',
+    },
+    {
+      id: 2,
+      city: 'London',
+      time: 'Tuesday',
+      hour: '1:40',
+    },
+    {
+      id: 3,
+      city: 'Vancouver',
+      time: 'Monday',
+      hour: '17:40',
+    },
+    {
+      id: 4,
+      city: 'Seoul',
+      time: 'Tuesday',
+      hour: '10:40',
+    },
+  ];
+
   return (
     <Widget size="large">
-      <WidgetContent className="flex-between gap-3">
-        <div className="w-full flex-center flex-col gap-2">
-          <p className="text-muted-foreground">
-            Mumbai
-          </p>
-          <WidgetTitle className="text-2xl">
-            Tuesday
-          </WidgetTitle>
-          <WidgetTitle className="text-4xl tracking-widest">
-            7:10
-          </WidgetTitle>
-        </div>
-        <div className="w-full flex-center flex-col gap-2">
-          <p className="text-muted-foreground">
-            London
-          </p>
-          <WidgetTitle className="text-2xl">
-            Tuesday
-          </WidgetTitle>
-          <WidgetTitle className="text-4xl tracking-widest">
-            1:40
-          </WidgetTitle>
-        </div>
-      </WidgetContent>
-      <WidgetContent className="flex-between gap-3">
-        <div className="w-full flex-center flex-col gap-2">
-          <p className="text-muted-foreground">
-            Vancouver
-          </p>
-          <WidgetTitle className="text-2xl">
-            Monday
-          </WidgetTitle>
-          <WidgetTitle className="text-4xl tracking-widest">
-            17:40
-          </WidgetTitle>
-        </div>
-        <div className="w-full flex-center flex-col gap-2">
-          <p className="text-muted-foreground">
-            Seoul
-          </p>
-          <WidgetTitle className="text-2xl">
-            Tuesday
-          </WidgetTitle>
-          <WidgetTitle className="text-4xl tracking-widest">
-            10:40
-          </WidgetTitle>
-        </div>
+      <WidgetContent className="grid grid-cols-2 gap-3">
+        {weatherInfo.map((info) => (
+          <div
+            key={info.id}
+            className="w-full flex-center flex-col gap-2"
+          >
+            <p className="text-muted-foreground">
+              {info.city}
+            </p>
+            <WidgetTitle className="text-2xl">
+              {info.time}
+            </WidgetTitle>
+            <WidgetTitle className="text-4xl tracking-widest">
+              {info.hour}
+            </WidgetTitle>
+          </div>
+        ))}
       </WidgetContent>
     </Widget>
   );
--- registry/default/docs/clock/clock-rectangle.tsx
+++ registry/default/docs/clock/clock-rectangle.tsx
@@ -5,31 +5,40 @@ import {
 } from '../../wigggle/widget';
 
 export default function ClockDemo() {
+  const weatherInfo = [
+    {
+      id: 1,
+      city: 'Mumbai',
+      time: 'Tuesday',
+      hour: '7:10',
+    },
+    {
+      id: 2,
+      city: 'London',
+      time: 'Tuesday',
+      hour: '1:40',
+    },
+  ];
+
   return (
     <Widget size="rectangle">
-      <WidgetContent className="flex-between gap-3">
-        <div className="w-full flex-center flex-col gap-2">
-          <p className="text-muted-foreground">
-            Mumbai
-          </p>
-          <WidgetTitle className="text-2xl">
-            Tuesday
-          </WidgetTitle>
-          <WidgetTitle className="text-5xl tracking-widest">
-            7:10
-          </WidgetTitle>
-        </div>
-        <div className="w-full flex-center flex-col gap-2">
-          <p className="text-muted-foreground">
-            London
-          </p>
-          <WidgetTitle className="text-2xl">
-            Tuesday
-          </WidgetTitle>
-          <WidgetTitle className="text-5xl tracking-widest">
-            1:40
-          </WidgetTitle>
-        </div>
+      <WidgetContent className="grid grid-cols-2 gap-3">
+        {weatherInfo.map((info) => (
+          <div
+            key={info.id}
+            className="w-full flex-center flex-col gap-2"
+          >
+            <p className="text-muted-foreground">
+              {info.city}
+            </p>
+            <WidgetTitle className="text-2xl">
+              {info.time}
+            </WidgetTitle>
+            <WidgetTitle className="text-4xl tracking-widest">
+              {info.hour}
+            </WidgetTitle>
+          </div>
+        ))}
       </WidgetContent>
     </Widget>
   );
